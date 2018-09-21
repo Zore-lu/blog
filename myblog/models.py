2d61360dcbc1203a61b465f540472075b2bf9348
qiyu <ismelu20@gmail.com>
2018-09-21 17:41:32 +0800

    update models

--- myblog/models.py
+++ myblog/models.py
@@ -3,4 +3,21 @@ from django.db import models
 # Create your models here.
 
 class Articles(models.Model):
-   pass
+    # https://zhangjia.tv/852.html
+
+    pass
+    # 博客id
+
+    # 用户id
+
+    # 博客标题
+
+    # 博客内容
+
+    # 浏览量
+
+    # 评论总数
+
+    # 发表日期
+
+    # 点赞数
